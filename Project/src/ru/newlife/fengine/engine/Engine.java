61c1afc9f85125ec65d39d97012c4478672c6be1
NewLife1324 <61694357+NewLife1324@users.noreply.github.com>
2020-12-26 19:32:42 +0300

    Quad render tests

--- Project/src/ru/newlife/fengine/engine/Engine.java
+++ Project/src/ru/newlife/fengine/engine/Engine.java
@@ -1,5 +1,16 @@
 package ru.newlife.fengine.engine;
 
+import java.nio.FloatBuffer;
+
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.system.MemoryUtil;
+import org.lwjgl.opengl.GL30;
+
+import ru.newlife.fengine.engine.input.Keyboard;
+import ru.newlife.fengine.engine.input.Mouse;
+
 public class Engine {
 	public static final int WIDTH = 640;
 	public static final int HEIGHT = 360;
@@ -17,14 +28,61 @@ public class Engine {
 		this.update();
 	}
 	
-	public  void update() {
-		while (!this.engineWindow.isCloseRequest()) {
+	public FloatBuffer storeDataInFloatBuffer(float[] data) {
+		FloatBuffer buffer = MemoryUtil.memAllocFloat(data.length);
+		buffer.put(data);		
+		buffer.flip();
+		return buffer;
+	}
+	public void update() 
+	{
+		float[] v_quad =
+			{
+
+				0.5f, 0.5f, 0,  -0.5f, 0.5f, 0,  -0.5f, -0.5f, 0,
+
+				0.5f, 0.5f, 0,  -0.5f, -0.5f, 0,  0.5f, -0.5f, 0
+			};
+		
+
+		int vaoId = GL30.glGenVertexArrays();
+		GL30.glBindVertexArray(vaoId);
+
+		int vboId = GL30.glGenBuffers();
+		GL30.glBindBuffer(GL30.GL_ARRAY_BUFFER, vboId);
+		
+		FloatBuffer fBuffer = this.storeDataInFloatBuffer(v_quad);
+		GL30.glBufferData(GL30.GL_ARRAY_BUFFER, fBuffer, GL30.GL_STATIC_DRAW);
+		MemoryUtil.memFree(fBuffer);
+		
+		GL30.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
+		
+		
+		GL30.glBindBuffer(GL30.GL_ARRAY_BUFFER, vboId);
+		GL30.glBindVertexArray(vaoId);
+	
+		while(!this.engineWindow.isCloseRequest())
+		{
+			Keyboard.handleKeyboardInput();
+			Mouse.handleMouseInput();
+			
+			GL11.glClearColor(0, 0.8f, 0, 1);
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+			
+			GL30.glBindVertexArray(vaoId);
+			
+			GL30.glEnableVertexAttribArray(0);
+
+			GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, v_quad.length / 3);
+
+			GL30.glDisableVertexAttribArray(0);
+			GL30.glBindVertexArray(vaoId);
+
 			this.engineWindow.update();
 		}
 		
 		this.engineWindow.destroy();
 	}
-	
 
 	
 }
